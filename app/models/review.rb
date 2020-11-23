2d71ed2b5d34b8753b4686f09c63c10622c12425
yilei jiang <yilei.jiang0@gmail.com>
2020-11-23 15:54:57 +0100

    added validation and relations in the models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :subscription
+  validates :rating, :comment, presence: true
+  validates :rating, inclusion: { in: 1..5 }
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,5 @@
 class Subscription < ApplicationRecord
   belongs_to :user
+  has_many :subscription_workshops
+  has_many :reviews
 end
