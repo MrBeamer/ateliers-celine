d0c080244a9736eed0417ba60a70b923bd44dd92
yilei jiang <yilei.jiang0@gmail.com>
2020-11-24 15:13:36 +0100

    bla

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_110408) do
+ActiveRecord::Schema.define(version: 2020_11_24_140833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_110408) do
     t.string "phone"
     t.string "street"
     t.string "username"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -117,6 +118,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_110408) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "origins_description"
     t.index ["user_id"], name: "index_workshops_on_user_id"
   end
 
