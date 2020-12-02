c62167ff978f67454cb50868ff9e3680f7f08baf
yilei jiang <yilei.jiang0@gmail.com>
2020-12-02 10:52:32 +0100

    .

--- app/javascript/components/timeline.js
+++ app/javascript/components/timeline.js
@@ -1,3 +1,17 @@
+// function buttonDone() {
+
+  
+//   const buttons = document.querySelectorAll(".timeline-button");
+ 
+//   buttons.forEach( (button, index) => {
+//     button.addEventListener("click", (event) => {
+//     const ball = document.querySelector(`#ball_${index+1}`);
+//     ball.classList.toggle("ball-done");
+//   })
+
+//   });
+// } 
+
 function buttonDone() {
 
   
@@ -5,12 +19,19 @@ function buttonDone() {
  
   buttons.forEach( (button, index) => {
     button.addEventListener("click", (event) => {
-    const ball = document.querySelector(`#ball_${index+1}`);
-    ball.classList.toggle("ball-done");
+    const bar = document.querySelector(".progress-bar");
+    bar.classList.toggle(`progress-bar_${index+1}`);
+    button.classList.toggle("button-minimal-thicker-pink")
+    if (button.innerHTML === "Reset Progress") {
+      button.innerHTML = "Mark as Done";
+    } else {
+      button.innerHTML = "Reset Progress";
+    }
   })
 
   });
-}
+} 
+
 
 
 export { buttonDone }
